c1e322af8aa18dcee371f8092a3a87158f8c2282
Bisher-d790 <bisher.d790@gmail.com>
2018-12-26 20:51:01 +0800

    Initial Commit

---
+++ Assets/Controller.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Controller : MonoBehaviour {
+
+    [SerializeField] private Item[] items;
+    [SerializeField] private Text selectedItemText;
+    [SerializeField] private Text insertedMoneyText;
+    [SerializeField] private Text logText;
+    private double logTimer = 0.0f;
+    // Money is in sens
+    [SerializeField] private int insertedMoney = 0;
+    private int selectedItem = 0;
+
+    public void Start()
+    {
+        logText.text = "";
+        insertedMoneyText.text = ((double)insertedMoney / 100).ToString();
+        for(int i=0;i<=items.Length;i++)
+        {
+            items[i].setID(i);
+        }
+    }
+
+    public void AddMoney(int insertedMoney)
+    {
+        this.insertedMoney += insertedMoney;
+        insertedMoneyText.text = ((double)this.insertedMoney / 100).ToString();
+    }
+
+    public void SelectItem()
+    {
+        this.selectedItem = int.Parse(selectedItemText.text);
+        if (selectedItem < 0 || selectedItem >= items.Length)
+        {
+            logText.text = "Please enter a valid number.";
+            logTimer = 3.0f;
+            return;
+        }
+        if (items[selectedItem].getPrice() <= insertedMoney)
+        {
+            AddMoney(-1 * items[selectedItem].SelectItem());
+        }
+        else
+        {
+            logText.text = "Not enough money!";
+            logTimer = 3.0f;
+        }
+    }
+
+    public void Update()
+    {
+        if (logTimer > 0)
+        {
+            logTimer -= Time.deltaTime;
+            if (logTimer < 1)
+            {
+                logTimer = 0;
+                logText.text = "";
+            }
+        }
+    }
+
+}
---
+++ Assets/Item.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+
+public class Item : MonoBehaviour
+{
+    [SerializeField] private Text itemNumber;
+    [SerializeField] private Text itemQuantity;
+    [SerializeField] private Text itemPrice;
+    private int ID = 0;
+    [SerializeField] private int quantity = 10;
+    // Money is in Sens
+    [SerializeField] private int price = 100;
+
+    public void Awake()
+    {
+        itemQuantity.text = quantity.ToString();
+        itemPrice.text = ((double) price/100).ToString();
+    }
+
+    public int getNumber()
+    {
+        return ID;
+    }
+
+    public int getQuantity()
+    {
+        return quantity;
+    }
+
+    public int getPrice()
+    {
+        return price;
+    }
+
+    public void setID(int ID)
+    {
+        this.ID = ID;
+        itemNumber.text = ID.ToString();
+    }
+
+    // removes 1 from the quantity of the item and returns the price of the item 
+    public int SelectItem()
+    {
+        quantity--;
+        itemQuantity.text = quantity.ToString();
+        return price;
+    }
+
+}
